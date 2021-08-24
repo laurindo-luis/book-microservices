bc541518c8281452cf5765a938f32a03219b7ec9
luiseduCosta <luiseduardocosta417@gmail.com>
2021-08-24 16:29:02 -0300

    upload project cambio-microservice

---
+++ cambio-microservice/src/main/java/br/com/luis/CambioMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.luis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CambioMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CambioMicroserviceApplication.class, args);
+	}
+
+}
---
+++ cambio-microservice/src/test/java/br/com/luis/CambioMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
