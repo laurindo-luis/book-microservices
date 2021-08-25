1989d53186f9b8f88e7506b285b1e47d29692616
luiseduCosta <luiseduardocosta417@gmail.com>
2021-08-24 22:33:16 -0300

    dependency jpa and flyway added and configuration of the file applications.yml

--- cambio-microservice/src/main/java/br/com/luis/cambio/CurrencyExchange.java
+++ cambio-microservice/src/main/java/br/com/luis/cambio/CurrencyExchange.java
@@ -3,6 +3,8 @@ package br.com.luis.cambio;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,13 +13,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 
 @Data
+@Entity
 public class CurrencyExchange implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private Long id;
-	private String from;
-	private String to;
+	private String fromCurrency;
+	private String toCurrency;
 	private BigDecimal conversionFactor;
 	private BigDecimal convertedValue;
 	private String environment;
