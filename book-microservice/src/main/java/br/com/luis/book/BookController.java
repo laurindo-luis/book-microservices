3ce83269a0f4b97f03d27e7460955e4a8f431026
Luis Eduardo <luiseduardocosta417@gmail.com>
2021-09-04 12:42:30 -0300

    rateLimiter

--- book-microservice/src/main/java/br/com/luis/book/BookController.java
+++ book-microservice/src/main/java/br/com/luis/book/BookController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -48,10 +49,12 @@ public class BookController {
 	
 	@GetMapping("/resilience4j")
 	//@Retry(name = "default", fallbackMethod = "fallbackMethod")
-	@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+	//@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+	@RateLimiter(name = "default")
 	public String getTestResilience4j() {
 		logger.info("Request book-service is received!");
-		return new RestTemplate().getForEntity("http://localhost:8080/book-service", String.class).getBody();
+		return "Request book-service is received!";
+		//return new RestTemplate().getForEntity("http://localhost:8080/book-service", String.class).getBody();
 	}
 	
 	public String fallbackMethod(Exception e) {
